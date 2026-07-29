64902e2d698e34c208cc91ffc621c29bd78bf176
Moeketsi Motaung <67917384+momotaun@users.noreply.github.com>
2026-07-29 17:05:05 +0200

    feat: add global PrismaModule, validation pipe, CORS, and exception filter (#37)

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -1,10 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
+const common_1 = require("@nestjs/common");
 const app_module_1 = require("./app.module");
+const all_exceptions_filter_1 = require("./common/filters/all-exceptions.filter");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    await app.listen(process.env.PORT ?? 3000);
+    app.useGlobalFilters(new all_exceptions_filter_1.AllExceptionsFilter());
+    app.setGlobalPrefix('api');
+    app.useGlobalPipes(new common_1.ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+    }));
+    app.enableCors({
+        origin: process.env.FRONTEND_URL ?? 'http://localhost:3000',
+        credentials: true,
+    });
+    await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
